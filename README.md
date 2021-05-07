b7db1393514a128144fec4c0162a86b8d7efd39a
Egor <cregordudes@gmail.com>
2021-05-07 11:35:44 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather App
+
+I used OpenWeatherMap API - https://openweathermap.org/ for the Weather and location data.
+
+How to display openweathermap weather icon - https://stackoverflow.com/questions/44177417/how-to-display-openweathermap-weather-icon/62369654#62369654
+
---
+++ script.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', ()=> {
+   let long;
+   let lat;
+
+   let temperatureDescription = document.querySelector('.temperature-description');
+   let temperatureDegree = document.querySelector('.temperature-degree');
+   let locationTimezone = document.querySelector('.location-timezone');
+   let locationIcon = document.querySelector('.weather-icon');
+   
+   if(navigator.geolocation){
+      navigator.geolocation.getCurrentPosition(position =>{
+         long = position.coords.longitude;
+         lat = position.coords.latitude;
+
+
+         const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=d0acafd4ded0084fd9db295f172a2cd4`;
+      
+         fetch(api)
+            .then(response =>{
+               return response.json();
+            })
+            .then(data =>{
+               //console.log(data);
+               const geolocation = (data.sys.country + ", " + data.name)
+               // Set DOM Elements form the API
+               temperatureDegree.textContent = data.main.temp;
+               temperatureDescription.textContent = data.weather[0].description; 
+               locationTimezone.textContent = geolocation;
+
+               const {icon} = data.weather[0];
+               locationIcon.innerHTML = `<img src="icons/${icon}.png">`;
+               //console.log(icon)
+            });
+      
+      });
+
+   }else{
+      alert = "Ooops, now i have no idea what region are you in :( "
+   }
+});
